8054a279dd35b0921244a787d1f80f4437b942b7
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-09 12:17:30 -0500

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 //% Components
 import Directory from "./components/directory/directory.component";
 
+import {Routes, Route, Link} from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </div>
+  );
+}
+
 const App = function () {
   const categories = [
     {
@@ -30,7 +60,15 @@ const App = function () {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
