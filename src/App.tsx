d776d9079e56f2e4c923261b03d2814fabf00b82
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-09 16:31:08 -0500

    installed firebase and added new nav link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //% Components
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 import {Routes, Route, Link, Outlet} from "react-router-dom";
 
@@ -11,6 +12,7 @@ const App = function () {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
