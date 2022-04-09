cc3f0925dcfcb6fe71ca691fa4aef6a3d15d2aa2
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-09 15:38:36 -0500

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,13 @@
 //% Components
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
 
 import {Routes, Route, Link} from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </div>
-  );
-}
-
 const App = function () {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <div>
-      <Directory categories={categories} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
