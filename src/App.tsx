3240f7147372cbda8c9287c8789c870a705b0214
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-09 16:05:09 -0500

    added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 //% Components
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route, Link, Outlet} from "react-router-dom";
 
 const Shop = () => <div>Im the shop</div>;
 const App = function () {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
