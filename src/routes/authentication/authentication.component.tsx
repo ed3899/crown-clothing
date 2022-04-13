2fc79d08736d1e45fa7c419b9db7742a2652990a
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-13 12:18:23 -0500

    added sign in auth

--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -1,23 +1,23 @@
+//% Libs
 import {
   signInWithGooglePopup,
   createUserDocFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+//% Components
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocFromAuth(user);
-  };
+const Authentication = () => {
+  
 
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
