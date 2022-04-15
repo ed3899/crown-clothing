9f4a564f43189aca993ea4a518158a1efee9e51d
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-15 10:07:50 -0500

    nested routes in shopping

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = function () {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
